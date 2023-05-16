1ed103a30357e9dc76bc9cd40eacc79e39ed5e06
khg1652 <129718188+khg1652@users.noreply.github.com>
2023-05-16 12:09:21 +0900

    Add files via upload

---
+++ brick.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	$("#start").click(function(){
+		$("#button_field").hide();
+		$(".interfaceMenu").show();
+	});
+
+	$("#setting").click(function(){
+		$("#button_field").hide();
+		change_position($(".popup"));
+		$("#setting_div").show();
+	});
+
+	$("#guide").click(function(){
+		$("#button_field").hide();
+		change_position($(".popup"));
+		$("#guide_div").show();
+	});
+
+	$("#story").click(function(){
+		$(".story_div").fadeIn();
+	});
+
+	$(".story_div").each(function(){
+		$(this).click(function(){
+			$(this).fadeOut();
+		});
+	});
+
+	$("#setting_ok").click(function(){
+		$("#setting_div").hide();
+		$("#button_field").show();
+	});
+
+	$("#guide_ok").click(function(){
+		$("#guide_div").hide();
+		$("#button_field").show();
+	});
+
+	function change_position(obj){
+		var l = ($(window).width()-obj.width())/2;
+		var t = ($(window).height()-obj.height())/2;
+		obj.css({top:t,left:l});
+	}
+	$(window).resize(function(){
+		change_position($(".popup"));
+	});
+
+
+});
