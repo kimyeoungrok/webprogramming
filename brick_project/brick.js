15acf51acda30d2557dbf7fa5de4163df2c9c294
kimyeoungrok <praoo800@konkuk.ac.kr>
2023-05-18 23:05:11 +0900

    level switch mode add

--- brick_project/brick.js
+++ brick_project/brick.js
@@ -54,6 +54,7 @@ $(document).ready(function(){
 		$(".interfaceMenu").show();
 		// 김영록 추가 수정
 		$("#mycanvas").show();
+		level_count = 1;
 		canvas_width = parseInt($("#mycanvas").attr("width"));
 		canvas_height = parseInt($("#mycanvas").attr("height"));
 		paddle_x = (canvas_width-paddle_width)/2; //paddle x축 위치
@@ -68,10 +69,36 @@ $(document).ready(function(){
 	$("#green").click(function(){
 		$("#stage").hide();
 		$(".interfaceMenu").show();
+		// 김영록 추가 수정
+		$("#mycanvas").show();
+		level_count = 2;
+		canvas_width = parseInt($("#mycanvas").attr("width"));
+		canvas_height = parseInt($("#mycanvas").attr("height"));
+		paddle_x = (canvas_width-paddle_width)/2; //paddle x축 위치
+		paddle_y = (canvas_height-100)-paddle_height; //paddle y축 위치
+
+		Ball_x = paddle_x + 100;//Ball의 초기 위치 및 재생성 위치는 paddle의 위
+		Ball_y = paddle_y - 50;
+		console.log(Ball_x);
+		console.log(Ball_y);
+		init();
 	});
 	$("#blue").click(function(){
 		$("#stage").hide();
 		$(".interfaceMenu").show();
+		// 김영록 추가 수정
+		$("#mycanvas").show();
+		level_count = 3;
+		canvas_width = parseInt($("#mycanvas").attr("width"));
+		canvas_height = parseInt($("#mycanvas").attr("height"));
+		paddle_x = (canvas_width-paddle_width)/2; //paddle x축 위치
+		paddle_y = (canvas_height-100)-paddle_height; //paddle y축 위치
+
+		Ball_x = paddle_x + 100;//Ball의 초기 위치 및 재생성 위치는 paddle의 위
+		Ball_y = paddle_y - 50;
+		console.log(Ball_x);
+		console.log(Ball_y);
+		init();
 	});
 
 	$("#setting").click(function(){
@@ -125,12 +152,12 @@ $(document).ready(function(){
 //김영록
 function init(){ // 맵 초기화
 	$(document).on("mousemove", mouseMoveHandler);
-	$(document).on("keydown",function(e){
-		if(e.key == " "){
-			start = !start;
-		}
-		console.log(e.key); //트러블 슈팅 : 한글키는 인식안됨
-	});
+	// $(document).on("keydown",function(e){
+	// 	if(e.key == " "){
+	// 		start = !start;
+	// 	}
+	// 	console.log(e.key); //트러블 슈팅 : 한글키는 인식안됨
+	// });
 	
 	canvas = document.getElementById("mycanvas");
 	context = canvas.getContext('2d');
@@ -141,6 +168,16 @@ function init(){ // 맵 초기화
 		itemR();
 		imagemakingR(); // 아이템 이미지 구현(게임 정보 란에있는 루비그림 투명화 작업 김영록)
 	}
+	else if(level_count == 2){
+		mapG();
+		itemG();
+		imagemakingG(); // 아이템 이미지 구현(게임 정보 란에있는 루비그림 투명화 작업 김영록)
+	}
+	else if(level_count == 3){
+		mapB();
+		itemB();
+		imagemakingB(); // 아이템 이미지 구현(게임 정보 란에있는 루비그림 투명화 작업 김영록)
+	}
 	
 	draw(); 
 	interval = setInterval(draw,20);
@@ -148,6 +185,18 @@ function init(){ // 맵 초기화
 }
 //1단계 아이템 이미지 구현
 function imagemakingR(){
+	for(var i=1; i<item_array.length; i++) {
+		$("#" + "img" + i).css("opacity","0.3");
+	}
+}
+
+//2단계 아이템 이미지 구현
+function imagemakingG(){
+	for(var i=1; i<item_array.length; i++) {
+		$("#" + "img" + i).css("opacity","0.3");
+	}
+}
+function imagemakingB(){
 	for(var i=1; i<item_array.length; i++) {
 		$("#" + "img" + i).css("opacity","0.3");
 	}
@@ -161,28 +210,56 @@ function draw(){
 		$(document).on("keydown",function(e){
 			if(e.key == " "){
 				start = !start;
-			}
-			if(life <= 0){
-				life = 3;
-				score = 0;
-				item_count = 0;
-				item_total = 0;
-				init();
-				$("#life h2").text("");
-				for(var i=0; i<life; i++) { // 생명 그림 나타나도록 구현
-				$("#life h2").append("♥");
+				if(life <= 0){
+					start = !start;
+					console.log("다시시작");
+					life = 3;
+					score = 0;
+					item_count = 0;
+					item_total = 0;
+					level_count = 1;
+					init();
+					$("#life h2").text("");
+					for(var i=0; i<life; i++) { // 생명 그림 나타나도록 구현
+						$("#life h2").append("♥");
+					}
+					$("#score h2").text(score); // 다시 시작시 점수, 생명, 먹은 아이템 개수 초기화
+					$("#item").text("아이템 : " + item_count + "/" + item_array.length/3);
 				}
-				$("#score h2").text(score); // 다시 시작시 점수, 생명, 먹은 아이템 개수 초기화
-				$("#item").text("아이템 : " + item_count + "/" + item_array.length/3);
+				else if(item_count >= item_total){
+					start = !start;
+					//볼 위치 수정
+					Ball_x = paddle_x + 100;
+					Ball_y = paddle_y - 50;
+					// 다음단계로 넘어갈시 공이 위로 뜨는 현상 제지하기 위함
+					Balldx = 5;
+					Balldy = 5;
+					console.log("다음단계");
+					item_count = 0;
+					item_total = 0;
+					init();
+					$("#life h2").text("");
+					for(var i=0; i<life; i++) { // 생명 그림 나타나도록 구현
+						$("#life h2").append("♥");
+					}
+					$("#item").text("아이템 : " + item_count + "/" + item_array.length/3);
+				}
+			
 			}
-			console.log(e.key); //트러블 슈팅 : 한글키는 인식안됨
+			
+			//console.log(e.key); //트러블 슈팅 : 한글키는 인식안됨
 		});
 	}
 	if(life <= 0) { // 생명이 고갈되면 게임오버 화면으로 전환
+		console.log("게임오버" + start);
 		gameover();
 	}
-	if(item_count >= item_total){ // 아이템을 모두 모으면 클리어 화면으로 전환
+	else if(item_count >= item_total){ // 아이템을 모두 모으면 클리어 화면으로 전환
+		console.log("게임클리어" + start + item_count + " : " + item_total);
+		//start = false;
+		level_count += 1;
 		gameclear();
+
 	}
 	else{
 		drawPaddle();
@@ -227,14 +304,37 @@ function makebrick(){
 }
 //김영록
 function makeitem(){
-	for(var i=0; i<item_array.length; i=i+3){
+	if(level_count == 1){ //1단계일때는 빨간색 아이템
+		for(var i=0; i<item_array.length; i=i+3){ 
 		if(item_array[i] == 1){
 			context.beginPath();
 			context.fillStyle = "red";
 			context.fillRect(item_array[i+1],item_array[i+2],item_width,item_height); 
-		}
+			}
 		
+		}
 	}
+	else if(level_count == 2){
+		for(var i=0; i<item_array.length; i=i+3){ 
+		if(item_array[i] == 1){
+			context.beginPath();
+			context.fillStyle = "green";
+			context.fillRect(item_array[i+1],item_array[i+2],item_width,item_height); 
+			}
+		
+		}
+	}
+	else if(level_count == 3){
+		for(var i=0; i<item_array.length; i=i+3){ 
+		if(item_array[i] == 1){
+			context.beginPath();
+			context.fillStyle = "blue";
+			context.fillRect(item_array[i+1],item_array[i+2],item_width,item_height); 
+			}
+		
+		}
+	}
+	
 }
 //김영록
 var paddlecolision = false; // 패들 충돌 감지
@@ -267,7 +367,7 @@ function moveBall(){
 			// var speedx = ((Ball_x)-((paddle_x + paddle_width)/2))*0.02;
 			// console.log(speedx);
 			Balldy = -Balldy;
-			Balldx = 1/Math.tan(45*(Math.PI/180) + (Ball_x - (paddle_width + paddle_x)/2)*0.44)*Balldy; // 보정 구현(아직 완벽하게 구현x 김영록)
+			//Balldx = 1/Math.tan(45*(Math.PI/180) + (Ball_x - (paddle_width + paddle_x)/2)*0.44)*Balldy; // 보정 구현(아직 완벽하게 구현x 김영록)
 			// if(Balldx < 0){
 			// 	Balldx = -Balldx;
 			// }
@@ -317,6 +417,9 @@ function moveBall(){
 					$("#item").text("아이템 : " + item_count + "/" + item_total);
 					//아이템 그림이 선명해지는 작업 추가 김영록
 					$("#" + "img" + item_count).css("opacity","1");
+					if(item_count >= item_total){
+						start = !start;
+					}
 				}
 			}
 		// context.beginPath();
@@ -334,14 +437,16 @@ function gameover(){ // 게임오버시 나타나는 창
 	clearInterval(interval);
 	context.clearRect(0,0,canvas_width,canvas_height); // 게임화면 지우기
 	context.font = 'italic 30pt Arial'
-	context.fillText("게임 오버 다시 시작하려면 스페이바 키를 눌러주세요!", canvas_width/6-150, canvas_height/2);
+	context.fillText("게임 오버 다시 시작하려면 스페이스바 키를 눌러주세요!", canvas_width/6-150, canvas_height/2);
 }
 //김영록
 function gameclear(){ // 게임 클리어시 나타나는 창
 	clearInterval(interval);
+
 	context.clearRect(0,0,canvas_width,canvas_height); // 게임화면 지우기
 	context.font = 'italic 30pt Arial'
-	context.fillText("게임클리어! 다음단계로 넘어가려면 (키)를 눌러주세요!", canvas_width/6-150, canvas_height/2);
+	context.fillText("게임클리어! 다음단계로 넘어가려면 스페이스바 를 눌러주세요!", canvas_width/6-150, canvas_height/2);
+
 }
 //김영록
 function mouseMoveHandler(e) {
@@ -464,7 +569,115 @@ function itemR(){ // 1단계 아이템 배치
 	item_total += 1;
 
 }
-function mapB(){ //2단계 벽돌배치
+function mapG(){ //2단계 벽돌배치
+	brick_x = 450;
+	brick_y = 30;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+	brick_x = 320;
+	brick_y = 80;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+	brick_x = 580;
+	brick_y = 80;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+	brick_x = 450;
+	brick_y = 140;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+	brick_x = 320;
+	brick_y = 200;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+	brick_x = 320;
+	brick_y = 270;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+	brick_x = 320;
+	brick_y = 340;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+	brick_x = 580;
+	brick_y = 200;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+	brick_x = 630;
+	brick_y = 250;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+	brick_x = 680;
+	brick_y = 300;
+	brick.push(1);
+	brick.push(brick_x);
+	brick.push(brick_y);
+	brick_count += 1;
+}
+//김영록
+function itemG(){ //2단계 아이템 배치
+	item_x = 300;
+	item_y = 20;
+	item_array.push(1);
+	item_array.push(item_x);
+	item_array.push(item_y);
+	item_total += 1;
+
+	item_x = 480;
+	item_y = 80;
+	item_array.push(1);
+	item_array.push(item_x);
+	item_array.push(item_y);
+	item_total += 1;
+
+	item_x = 700;
+	item_y = 100;
+	item_array.push(1);
+	item_array.push(item_x);
+	item_array.push(item_y);
+	item_total += 1;
+
+	item_x = 480;
+	item_y = 300;
+	item_array.push(1);
+	item_array.push(item_x);
+	item_array.push(item_y);
+	item_total += 1;
+
+	item_x = 250;
+	item_y = 330;
+	item_array.push(1);
+	item_array.push(item_x);
+	item_array.push(item_y);
+	item_total += 1;
+
+	item_x = 700;
+	item_y = 400;
+	item_array.push(1);
+	item_array.push(item_x);
+	item_array.push(item_y);
+	item_total += 1;
+
+}
+
+function mapB(){ //3단계 벽돌배치
 	brick_x = 450;
 	brick_y = 30;
 	brick.push(1);
